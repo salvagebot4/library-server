f4c1d9612d48b3c7c69d1826ac7fd90ff51d3b57
KevinZheng14815 <91815800+KevinZheng14815@users.noreply.github.com>
2023-04-23 16:08:17 -0500

    Changed the get routes

--- Models/checkouthistoryModel.js
+++ Models/checkouthistoryModel.js
@@ -4,7 +4,10 @@ class CheckoutHistory {
     static async getAllCheckoutHistories() {
         try {
             const result = await pool.query(`
-                SELECT * FROM postgres.library.checkout_history;
+            SELECT  p.product_name, pi.*, c.checkout_id 
+            FROM postgres.library.checkout_history c, postgres.library.product_instance pi, postgres.library.product p
+            WHERE c.instance_id = pi.instance_id AND
+            pi.product_id = p.product_id;
             `)
             return result.rows;
 
